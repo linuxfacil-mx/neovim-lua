9d4361c3ae9ef54dc01fab58059d3602251ee3c1
Linux Facil <jramirez.sanchez@gmail.com>
2022-07-02 17:33:45 -0500

    settings

---
+++ lua/settings/init.lua
@@ -0,0 +1,21 @@
+local set = vim.opt
+
+set.termguicolors = true
+set.cursorline = true
+set.number = true
+set.relativenumber = true
+set.wrap = false
+
+set.encoding = "UTF-8"
+
+set.smarttab = true
+set.expandtab = true
+set.tabstop = 2
+set.softtabstop = 2
+set.shiftwidth = 2
+set.smartindent = true
+
+set.swapfile = false
+set.backup = false
+set.undofile = true
+set.undodir = vim.fn.stdpath("data") .. "/undodir"

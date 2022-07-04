dffffd93f2f4bf7eabc5efc965fb677139e89b3d
LinuxFacil.mx <76665249+linuxfacil-mx@users.noreply.github.com>
2022-07-03 21:12:47 -0500

    Update init.lua

--- lua/settings/init.lua
+++ lua/settings/init.lua
@@ -2,19 +2,31 @@ local set = vim.opt
 
 set.termguicolors = true
 set.cursorline = true
+set.laststatus = 3
+
+set.hidden = true
+set.wrap = false
+set.encoding = "UTF-8"
+
 set.number = true
 set.relativenumber = true
-set.wrap = false
 
-set.encoding = "UTF-8"
-set.hidden = true
-
+set.smartindent = true
 set.smarttab = true
-set.expandtab = true
 set.tabstop = 2
 set.softtabstop = 2
 set.shiftwidth = 2
-set.smartindent = true
+set.expandtab = true
+set.smartcase = true
+
+set.splitbelow = true
+set.splitright = true
+set.scrolloff = 8
+set.incsearch = true
+set.hlsearch = false
+set.signcolumn = "yes"
+set.colorcolumn = "80"
+vim.highlight.create("ColorColumn", { ctermbg = 0, guibg = "lightgrey" }, false)
 
 set.swapfile = false
 set.backup = false
